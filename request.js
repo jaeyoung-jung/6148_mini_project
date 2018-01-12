e808e99c0c5d80c60930bfc5eddbe790ab51b3d7
Nikhil Reddy <nikhilreddy6547@gmail.com>
2018-01-12 18:01:58 -0500

    Fixed more bugs

--- request.js
+++ request.js
@@ -9,23 +9,26 @@ $.ajax({
         console.log(json.data);
         result='';
         for (i in json.data) {
-            result += handleComment(i.comment);
+            result += handleComment(i);
         }
         $(".comments").html(result);
     })
     .fail(function() {
         $(".comments").html('<p class="error">Error: Unable to connect to comment.php</p>');
     });
-function handleComment() {
-    //To Do
+function handleComment(comment) {
+    return '<div><div class="name"> Posted by' + name + ':</div><div class="comment">' + comment + '</div></div>';
 }
 
 function submitComment() {
     console.log("HI");
     commentData = {
-        comment : $("#submit").html(),
-    }
-    console.log($("#submit").html());
+        name : "Anon",
+        email : "Anon@boards.4chan.org",
+        comment : $("#submit").val(),
+    };
+    console.log($("#submit").val());
+    $("#submit").val("");
     $.ajax({
         method : 'POST',
         data : commentData,
