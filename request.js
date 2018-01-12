bb831419874a662929f3823ae18c5f530337e28e
Jaeyoung Jung <jaeyoung.jung@protonmail.com>
2018-01-12 18:47:40 -0500

    Add support for blank emails

--- request.js
+++ request.js
@@ -1,4 +1,4 @@
-$.ajax({
+update=function(){$.ajax({
     method : 'GET',
     data : {},
     dataType : "json",
@@ -16,16 +16,19 @@ $.ajax({
     })
     .fail(function() {
         $("#comments").html('<div class="ui error message"><h4 class="ui header">There was an error connecting to the PHP server.</h4><p>Contact a Network Adminstrator if this problem persists.</p></div>');
-    });
+    });};
 function handleComment(i) {
-    return '<div class="ui raised segment"><h4 class="ui header"> Posted by: <a href="mailto:' + i.email + '">' + i.name + '</a></h4><p>' + i.comment + '</p></div>';
+    return '<div class="ui raised segment"><h4 class="ui header"> Posted by: ' + (i.email ? ('<a href="mailto:' + i.email + '">' + i.name + '</a>') : i.name) + '</h4><p>' + i.comment + '</p></div>';
 }
+
+update();
+window.setInterval(update,5000);
 
 function submitComment() {
     console.log("HI");
     commentData = {
         name : $("#name").val() || "Anon",
-        email : $("#email").val() || "Anon@boards.4chan.org",
+        email : $("#email").val(),
         comment : $("#submit").val(),
     };
     console.log(commentData);
