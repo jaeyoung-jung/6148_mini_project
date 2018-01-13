5bb2cb1780d739d23e93b8d6fc6a9c339859af2a
Nikhil Reddy <nikhilreddy6547@gmail.com>
2018-01-12 22:10:10 -0500

    Initial Commit

--- request.js
+++ request.js
@@ -1,45 +1,47 @@
-update=function(){$.ajax({
-    method : 'GET',
-    data : {},
-    dataType : "json",
-    url: "viewcomments.php",
-    cache: false,
-})
-    .done(function(json) {
-        console.log(json.data);
-        result='';
-        for (i in json.data) {
-            //console.log();
-            result = handleComment(json.data[i]) + result;
+angular.module('todoApp', [])
+    .controller('requestController', function() {
+        this.loaded = "";
+        this.failed = "hidden";
+        this.update = function() {
+            $.ajax({
+                method : 'GET',
+                data : {},
+                dataType : "json",
+                url: "viewcomments.php",
+                cache: false,
+            })
+            .done(function(json) {
+                const data = json.data;
+                console.log(data);
+                for (i = 0; i < data.length; i++) {
+                    data[i].url = data[i].email ? '<a href="mailto:{{data.email}}">{{data.name}}</a>' : '{{data.name}}';
+                }
+                this.comments = json.data;
+            })
+            .fail(function() {
+                this.failed = "";
+            })
+            .always(function() {
+                this.loaded = "hidden";
+            });
         }
-        $("#comments").html(result);
-    })
-    .fail(function() {
-        $("#comments").html('<div class="ui error message"><h4 class="ui header">There was an error connecting to the PHP server.</h4><p>Contact a Network Adminstrator if this problem persists.</p></div>');
-    });};
-function handleComment(i) {
-    return '<div class="ui raised segment"><h4 class="ui header"> Posted by: ' + (i.email ? ('<a href="mailto:' + i.email + '">' + i.name + '</a>') : i.name) + '</h4><p>' + i.comment + '</p></div>';
-}
-
-update();
-window.setInterval(update,5000);
-
-function submitComment() {
-    console.log("HI");
-    commentData = {
-        name : $("#name").val() || "Anon",
-        email : $("#email").val(),
-        comment : $("#submit").val(),
-    };
-    console.log(commentData);
-    //console.log($("#submit").val());
-    $("#name").val("");
-    $("#email").val("");
-    $("#submit").val("");
-    $.ajax({
-        method : 'POST',
-        data : commentData,
-        dataType : "json",
-        url: "addcomment.php",
-    }).done(console.log).fail(console.log);
-}
+        this.update();
+        window.setInterval(this.update, 5000);
+        this.submitComment() {
+            commentData = {
+                name : this.name || "Anon",
+                email : this.email,
+                comment : this.comment,
+            };
+            console.log(commentData);
+            this.name = "";
+            this.email = "";
+            this.comment = "";
+            $.ajax({
+                method : 'POST',
+                data : commentData,
+                dataType : "json",
+                url: "addcomment.php",
+            }).done(console.log).fail(console.log);
+        }
+    });
